ccf236b44692c783f1bd3916929c98c88c8b576e
Yuri Alves <yurialvesedf@gmail.com>
2022-10-19 12:30:56 -0300

    Adicionando comentarios explicativos

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -8,16 +8,28 @@ const SearchBar = (props) => {
     const [search, setSearch] = useState();
     const [pokemon, setPokemon] = useState();
 
+    //função chamada no momento em que o pokemon é digitado no input
     const onChangeEvent = (e) => {
+
+        //adicionando o resultado ao state criado para a busca
         setSearch(e.target.value);
     };
 
+
+    //função chamada no momento em que o botão é clicado
     const onClickEvent = () => {
+
+        //função para buscar o pokemon na API enviando como parametro o que foi digitado
         onSearchEvent(search);
     };
 
+    //função para chamar a API e retornar o resultado da busca
     const onSearchEvent = async (pokemon) => {
+
+        //resultado da função onde chamamos a API enviando um pokemon
         const result = await searchPokemonApi(pokemon);
+
+        //adicionando o resultado ao state criado para o pokemon
         setPokemon(result);
 
     };
--- src/pokeapi.js
+++ src/pokeapi.js
@@ -1,7 +1,8 @@
 export const searchPokemonApi = async (pokemon) => {
     try {
-
+        //atribuindo a uma variavel o link da api
         let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
         const response = await fetch(url);
         return await response.json();
 
